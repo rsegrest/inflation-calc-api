c492d7570bd9e5e81ea737be5c2e854aba79e835
Rick Segrest <rsegrest77@gmail.com>
2026-02-07 20:11:50 -0600

    Fixed minor bug

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from util.formulas import calculate_timespan_months
@@ -11,7 +11,6 @@ from util.date_helpers import get_current_year_int, get_current_month_int
 
 app = Flask(__name__)
 CORS(app)
-# CORS(app, origins=["http://localhost:3000", "http://127.0.0.1:3000"])
 
 @app.route("/")
 
@@ -24,35 +23,36 @@ def get_example():
     return response
 
 # @app.route('/cpi/<from_year>/<to_year>/<from_month>/<to_month>')
-@app.route('/cpi/<starting_amount>/<from_year>', defaults={'to_year': get_current_year_int(), 'from_month': 0, 'to_month': get_current_month_int()})
-@app.route('/cpi/<starting_amount>/<from_year>/<to_year>', defaults={'from_month': 0, 'to_month': 0})
-@app.route('/cpi/<starting_amount>/<from_year>/<to_year>/<from_month>', defaults={'to_month': 0})
-@app.route('/cpi/<starting_amount>/<from_year>/<to_year>/<from_month>/<to_month>')
-def cpi(starting_amount=1000, from_year=2022, to_year=2025, from_month=0, to_month=0):
+@app.route('/cpi', methods=['GET', 'POST'])
+def cpi():
+    if request.method == 'POST':
+        data = request.get_json()
+        starting_amount = data['starting_amount']
+        from_year = data['from_year']
+        to_year = data['to_year']
+        from_month = data['from_month']
+        to_month = data['to_month']
+    else:
+        starting_amount = request.args.get('starting_amount')
+        from_year = request.args.get('from_year')
+        to_year = request.args.get('to_year')
+        from_month = request.args.get('from_month')
+        to_month = request.args.get('to_month')
     starting_amount = int(starting_amount)
     from_year = int(from_year)
     to_year = int(to_year)
     from_month = int(from_month)
     to_month = int(to_month)
-    if from_year > to_year:
-        from_year = to_year
-    if from_month > 11:
-        from_month = 11
-    if to_month > 11:
-        to_month = 11
     if from_year < 1913:
         from_year = 1913
     if to_year < 1913:
         to_year = 1913
-    # return {
-    #     'starting_amount': starting_amount,
-    #     'from_year': from_year,
-    #     'to_year': to_year,
-    #     'from_month': from_month,
-    #     'to_month': to_month,
-    # }
     latest_data = get_latest_month_with_data(to_month, to_year)
-    now_dollars = adjust_figure_with_inflation(starting_amount, get_historical_cpi(from_year, from_month), get_historical_cpi(to_year, to_month))
+    from_cpi = get_historical_cpi(from_year, from_month)
+    to_month = latest_data['month']
+    to_year = latest_data['year']
+    to_cpi = get_historical_cpi(to_year, to_month)
+    now_dollars = adjust_figure_with_inflation(starting_amount, from_cpi, to_cpi)
     lost_value = calculate_loss_percentage(get_historical_cpi(from_year, from_month), get_historical_cpi(to_year, to_month))
     historical_start_cpi = get_historical_cpi(from_year, from_month)
     historical_end_cpi = get_historical_cpi(to_year, to_month)
@@ -74,10 +74,7 @@ def cpi(starting_amount=1000, from_year=2022, to_year=2025, from_month=0, to_mon
         'inflation_rate': inflation_rate,
         'time_period_in_months': time_period_in_months,
     }
+    return 'None'
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-#     const [timePeriodInMonths, setTimePeriodInMonths] = React.useState(calculateTimespanMonths(startYear, startZeroBasedMonth, endYear, endZeroBasedMonth));
-#     const [lostValue, setLostValue] = React.useState(calculateLossPercentage(historicalStartCpi, historicalEndCpi));
